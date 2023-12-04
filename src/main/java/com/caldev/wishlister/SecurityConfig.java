bcb9b71fe741f933a7020f605eda29bfa36efecc
Callum Batten-Plowright <callum.battenplowright@outlook.com>
2023-12-04 00:27:53 +0000

    fixed test passing bug by adding security config to application

---
+++ src/main/java/com/caldev/wishlister/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.caldev.wishlister;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/wishlists/**")
+                        .hasRole("USER"))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
