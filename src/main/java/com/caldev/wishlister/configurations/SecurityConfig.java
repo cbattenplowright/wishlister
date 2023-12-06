1467eac364495a6aab5d922dca6161c3ff0b41d8
Callum BP <callumbp15@gmail.com>
2023-12-06 15:56:58 +0000

    edited security config for mock user for tests

--- src/main/java/com/caldev/wishlister/configurations/SecurityConfig.java
+++ src/main/java/com/caldev/wishlister/configurations/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +24,21 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails bob = users
+                .username("bob123")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(bob);
     }
 }
