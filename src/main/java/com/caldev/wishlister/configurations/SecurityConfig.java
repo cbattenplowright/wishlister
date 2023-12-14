d16f75e6259d0f82e26a04789d00658b4f866433
Callum BP <callumbp15@gmail.com>
2023-12-14 19:08:04 +0000

    WIS-59 added jpaUserDetailsService to SecurityConfig forgot to add

--- src/main/java/com/caldev/wishlister/configurations/SecurityConfig.java
+++ src/main/java/com/caldev/wishlister/configurations/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.caldev.wishlister.configurations;
 
+import com.caldev.wishlister.services.JpaUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,11 +16,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final JpaUserDetailsService jpaUserDetailsService;
+
+    public SecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
+        this.jpaUserDetailsService = jpaUserDetailsService;
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/wishlists/**").hasRole("USER"))
+                .userDetailsService(jpaUserDetailsService)
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
--- src/main/java/com/caldev/wishlister/services/JpaUserDetailsService.java
+++ src/main/java/com/caldev/wishlister/services/JpaUserDetailsService.java
@@ -5,7 +5,9 @@ import com.caldev.wishlister.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JpaUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
