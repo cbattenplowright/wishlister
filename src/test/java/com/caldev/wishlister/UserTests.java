0ff30507927cf67706f72a114021737d2b9955c1
Callum BP <callumbp15@gmail.com>
2023-12-23 01:12:51 +0000

    WIS-21 refactored user tests into groups according to endpoints

--- src/test/java/com/caldev/wishlister/UserTests.java
+++ src/test/java/com/caldev/wishlister/UserTests.java
@@ -3,6 +3,7 @@ package com.caldev.wishlister;
 import com.caldev.wishlister.models.User;
 import jakarta.transaction.Transactional;
 import org.apache.coyote.Response;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,60 +19,67 @@ public class UserTests {
     @Autowired
     TestRestTemplate restTemplate;
 
-    @Test
-    void shouldReturnUserIfUserIsAuthenticatedAndOwner() {
-        ResponseEntity<User> response = restTemplate
-                .withBasicAuth("Bob123", "abc123")
-                .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", User.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody().getName()).isEqualTo("Bob");
+    @Nested
+    class showUserTests {
+        @Test
+        void shouldReturnUserIfUserIsAuthenticatedAndOwner() {
+            ResponseEntity<User> response = restTemplate
+                    .withBasicAuth("Bob123", "abc123")
+                    .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", User.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response.getBody().getName()).isEqualTo("Bob");
+        }
+
+        @Test
+        void shouldReturn403IfUserIsAuthenticatedAndNotOwner() {
+            ResponseEntity<String> response = restTemplate
+                    .withBasicAuth("Alice456", "xyz789")
+                    .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+        }
+
+        @Test
+        void shouldReturn404IfUserDoesNotExist(){
+            ResponseEntity<String> response = restTemplate
+                    .withBasicAuth("David012", "ghi789")
+                    .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74688", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+
+        @Test
+        void shouldReturnUserIfUserIsAdmin(){
+            ResponseEntity<User> response = restTemplate
+                    .withBasicAuth("David012", "ghi789")
+                    .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", User.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response.getBody().getName()).isEqualTo("Bob");
+        }
     }
 
-    @Test
-    void shouldReturn403IfUserIsAuthenticatedAndNotOwner() {
-        ResponseEntity<String> response = restTemplate
-                .withBasicAuth("Alice456", "xyz789")
-                .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    @Nested
+    class indexUserTests{
+        @Test
+        void shouldReturnCollectionOfUsersIfAdmin(){
+            ResponseEntity<String> response = restTemplate
+                    .withBasicAuth("David012", "ghi789")
+                    .getForEntity("/users", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
+
+        @Test
+        void shouldReturn403IfUserIsNotAuthorizedToViewAllUsers(){
+            ResponseEntity<String> response = restTemplate
+                    .withBasicAuth("Alice456", "xyz789")
+                    .getForEntity("/users", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+        }
+
+        @Test
+        void shouldReturn401IfUserIsNotAuthenticatedToViewAllUsers(){
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity("/users", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        }
     }
 
-    @Test
-    void shouldReturn404IfUserDoesNotExist(){
-        ResponseEntity<String> response = restTemplate
-                .withBasicAuth("David012", "ghi789")
-                .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74688", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-    }
-
-    @Test
-    void shouldReturnUserIfUserIsAdmin(){
-        ResponseEntity<User> response = restTemplate
-                .withBasicAuth("David012", "ghi789")
-                .getForEntity("/users/9886bb64-a584-46f0-aca4-10e3dec74458", User.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody().getName()).isEqualTo("Bob");
-    }
-
-    @Test
-    void shouldReturnCollectionOfUsersIfAdmin(){
-        ResponseEntity<String> response = restTemplate
-                .withBasicAuth("David012", "ghi789")
-                .getForEntity("/users", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    }
-
-    @Test
-    void shouldReturn403IfUserIsNotAuthorizedToViewAllUsers(){
-        ResponseEntity<String> response = restTemplate
-                .withBasicAuth("Alice456", "xyz789")
-                .getForEntity("/users", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
-    }
-
-    @Test
-    void shouldReturn401IfUserIsNotAuthenticatedToViewAllUsers(){
-        ResponseEntity<String> response = restTemplate
-                .getForEntity("/users", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
-    }
 }
