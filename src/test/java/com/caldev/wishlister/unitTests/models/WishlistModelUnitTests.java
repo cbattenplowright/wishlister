66bd8935bb3ed97eb1761da1d2604329f8c02ba9
Callum BP <callumbp15@gmail.com>
2023-12-31 13:09:20 +0000

    added unit tests for WishlistModel

---
+++ src/test/java/com/caldev/wishlister/unitTests/models/WishlistModelUnitTests.java
@@ -0,0 +1,64 @@
+package com.caldev.wishlister.unitTests.models;
+
+import com.caldev.wishlister.models.User;
+import com.caldev.wishlister.models.Wishlist;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WishlistModelUnitTests {
+
+    private Wishlist wishlist;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        // Create a sample wishlist for testing
+        user = new User("john_doe", "password123", "John Doe", "john.doe@example.com", LocalDate.of(1990, 1, 1));
+        wishlist = new Wishlist("My Wishlist", user);
+    }
+
+    @Test
+    void getWishlistId() {
+        // Test getters
+        assertEquals(null, wishlist.getWishlistId());
+    }
+
+    @Test
+    void getWishlistName() {
+        assertEquals("My Wishlist", wishlist.getWishlistName());
+
+    }
+
+    @Test
+    void getUser() {
+        assertEquals(user, wishlist.getUser());
+
+
+    }
+
+    @Test
+    void setWishlistId() {
+        // Test setters
+        Long newWishlistId = 1L;
+        wishlist.setWishlistId(newWishlistId);
+        assertEquals(newWishlistId, wishlist.getWishlistId());
+    }
+
+    @Test
+    void setWishlistName() {
+        wishlist.setWishlistName("My New Wishlist");
+        assertEquals("My New Wishlist", wishlist.getWishlistName());
+    }
+
+    @Test
+    void setUser() {
+        User newUser = new User("jane_doe", "password123", "Jane Doe", "jane.doe@example.com", LocalDate.of(1995, 1, 1));
+        wishlist.setUser(newUser);
+        assertEquals(newUser, wishlist.getUser());
+    }
+}
