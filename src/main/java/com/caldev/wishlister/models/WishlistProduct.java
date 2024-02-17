3e24bde97d4f138c965a24d8b695afabafdf4145
Callum Batten-Plowright <callum.battenplowright@outlook.com>
2024-02-17 23:23:44 +0000

    WIS-28 WIS-50 created WishlistProduct model

--- src/main/java/com/caldev/wishlister/models/WishlistProduct.java
+++ src/main/java/com/caldev/wishlister/models/WishlistProduct.java
@@ -1,2 +1,74 @@
-package com.caldev.wishlister.models;public class WishlistProduct {
+package com.caldev.wishlister.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "wishlist_products")
+public class WishlistProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wishlist_product_id")
+    private Long wishlistProductId;
+
+    @Column
+    private Wishlist wishlist;
+
+    @Column
+    private Product product;
+
+    @Column(name = "is_purchased")
+    private boolean isPurchased;
+
+    public WishlistProduct() {
+    }
+
+    public WishlistProduct(Long wishlistProductId, Wishlist wishlist, Product product) {
+        this.wishlistProductId = wishlistProductId;
+        this.wishlist = wishlist;
+        this.product = product;
+        this.isPurchased = false;
+    }
+
+    public Long getWishlistProductId() {
+        return wishlistProductId;
+    }
+
+    public void setWishlistProductId(Long wishlistProductId) {
+        this.wishlistProductId = wishlistProductId;
+    }
+
+    public Wishlist getWishlist() {
+        return wishlist;
+    }
+
+    public void setWishlist(Wishlist wishlist) {
+        this.wishlist = wishlist;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public boolean isPurchased() {
+        return isPurchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        isPurchased = purchased;
+    }
+
+    @Override
+    public String toString() {
+        return "WishlistProduct{" +
+                "wishlistProductId=" + wishlistProductId +
+                ", wishlist=" + wishlist +
+                ", product=" + product +
+                ", isPurchased=" + isPurchased +
+                '}';
+    }
 }
