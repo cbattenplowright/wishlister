1e7cdd541bfa0125bf76e449c58ce6b8a4e45a41
Callum Batten-Plowright <callum.battenplowright@outlook.com>
2024-03-18 14:40:06 +0000

    WIS-28 WIS-54 added WishlistProductDTO

---
+++ src/main/java/com/caldev/wishlister/models/WishlistProductDTO.java
@@ -0,0 +1,53 @@
+package com.caldev.wishlister.models;
+
+public class WishlistProductDTO {
+
+    private Long wishlistId;
+
+    private Long productId;
+
+    private boolean isPurchased;
+
+    public WishlistProductDTO() {
+
+    }
+
+    public WishlistProductDTO(Long wishlistId, Long productId, boolean isPurchased) {
+        this.wishlistId = wishlistId;
+        this.productId = productId;
+        this.isPurchased = isPurchased;
+    }
+
+    public Long getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(Long wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public boolean isPurchased() {
+        return isPurchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        isPurchased = purchased;
+    }
+
+    @Override
+    public String toString() {
+        return "WishlistProductDTO{" +
+                "wishlistId=" + wishlistId +
+                ", productId=" + productId +
+                ", isPurchased=" + isPurchased +
+                '}';
+    }
+}
