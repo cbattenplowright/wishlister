050620538fcf0515d11a7f06cabd1ab91a6ce21f
Callum BP <callumbp15@gmail.com>
2024-06-24 21:49:10 +0100

    start over

---
+++ src/main/java/com/caldev/wishlister/WishlisterApplication.java
@@ -0,0 +1,13 @@
+package com.caldev.wishlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlisterApplication.class, args);
+	}
+
+}
