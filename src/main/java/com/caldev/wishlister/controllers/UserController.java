d8bd97a228f8b52f7eb4954f1bf19eb21787159d
Callum BP <callumbp15@gmail.com>
2024-07-05 13:49:14 +0100

    written POST mapping for UserController

--- src/main/java/com/caldev/wishlister/controllers/UserController.java
+++ src/main/java/com/caldev/wishlister/controllers/UserController.java
@@ -1,13 +1,11 @@
 package com.caldev.wishlister.controllers;
 
+import com.caldev.wishlister.dtos.UserDto;
 import com.caldev.wishlister.services.UserService;
 import com.caldev.wishlister.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,14 +17,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+//    INDEX Users
     @GetMapping()
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+//    SHOW User
     @GetMapping("/{requestedId}")
     public ResponseEntity<User> getUserById(@PathVariable UUID requestedId){
         return ResponseEntity.ok(userService.getUserById(requestedId));
+    }
+
+//    CREATE User
+    @PostMapping("/new")
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto){
+        User user = userService.createUser(userDto);
+        return ResponseEntity.ok(user);
     }
 }
