27e1b7e83e79dae6cb22ff2c817f91ef5438f6ec
Callum BP <callumbp15@gmail.com>
2024-07-05 13:35:57 +0100

    added @PathVariable annotation to SHOW User endpoint

--- src/main/java/com/caldev/wishlister/controllers/UserController.java
+++ src/main/java/com/caldev/wishlister/controllers/UserController.java
@@ -5,10 +5,12 @@ import com.caldev.wishlister.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/users")
@@ -24,7 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/{requestedId}")
-    public ResponseEntity<User> getUserById(Long requestedId){
+    public ResponseEntity<User> getUserById(@PathVariable UUID requestedId){
         return ResponseEntity.ok(userService.getUserById(requestedId));
     }
 }
