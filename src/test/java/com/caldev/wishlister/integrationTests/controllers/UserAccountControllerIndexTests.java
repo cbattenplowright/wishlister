1728e165a3b9c97f6619fd6d50e1ca2c602e853f
Callum BP <callumbp15@gmail.com>
2024-07-30 08:31:53 +0100

    written INDEX endpoint tests

---
+++ src/test/java/com/caldev/wishlister/integrationTests/controllers/UserAccountControllerIndexTests.java
@@ -0,0 +1,82 @@
+package com.caldev.wishlister.integrationTests.controllers;
+
+
+import com.caldev.wishlister.controllers.UserController;
+import com.caldev.wishlister.entities.UserAccount;
+import com.caldev.wishlister.security.CustomUserDetailsService;
+import com.caldev.wishlister.security.SecurityConfig;
+import com.caldev.wishlister.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(controllers = UserController.class)
+public class UserAccountControllerIndexTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomUserDetailsService customUserDetailsService;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    UserAccount userAccount;
+
+    @BeforeEach
+    void setUp(){
+        when(userService.getAllUsers()).thenReturn(List.of(userAccount));
+    }
+
+
+    //   INDEX User Endpoint Tests
+    @Test
+    @WithMockUser   // Annotation simulates the user logging in
+    void whenUnAuthorizedAndRequestingAllUsers_thenReturn403() throws Exception {
+
+        this.mockMvc.perform(get("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenUnAuthenticatedAndRequestingUser_thenReturn401() throws Exception {
+
+        this.mockMvc.perform(get("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void whenAuthenticatedAndAuthorizedAndRequestingAllUsers_thenReturn200() throws Exception {
+
+        this.mockMvc.perform(get("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(result -> result.getResponse().getContentAsString().contains(userAccount.toString()));
+
+    }
+
+}
