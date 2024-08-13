761a278a3221a5b863903f579111141ea80afc0e
Callum BP <callumbp15@gmail.com>
2024-08-13 15:31:39 +0100

    WIS-33-delete-user-endpoint Written 404 delete user endpoint test GREEN and 200 delete user endpoint GREEN

--- src/test/java/com/caldev/wishlister/integrationTests/controllers/UserAccountControllerDeleteUserTests.java
+++ src/test/java/com/caldev/wishlister/integrationTests/controllers/UserAccountControllerDeleteUserTests.java
@@ -1,17 +1,28 @@
 package com.caldev.wishlister.integrationTests.controllers;
 
 import com.caldev.wishlister.controllers.UserController;
+import com.caldev.wishlister.entities.Authority;
 import com.caldev.wishlister.entities.UserAccount;
+import com.caldev.wishlister.exceptions.UserNotFoundException;
 import com.caldev.wishlister.security.CustomUserDetailsService;
 import com.caldev.wishlister.security.SecurityConfig;
 import com.caldev.wishlister.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,13 +39,33 @@ public class UserAccountControllerDeleteUserTests {
     @MockBean
     UserService userService;
 
-    @MockBean
-    UserAccount userAccount;
+    UserAccount testUserAccount;
+
+    @BeforeEach
+    void setUp(){
+        testUserAccount = new UserAccount("test@email.com", "test", "test", LocalDate.now(), new HashSet<>(List.of(new Authority("ROLE_ADMIN"))));
+    }
 
     @Test
     void whenUserIsAuthenticatedAndAuthorized_thenReturn200() throws Exception {
-
-        this.mockMvc.perform(delete("/api/users/delete"))
+        UUID testUserId = UUID.randomUUID();
+        doNothing().when(userService).deleteUser(testUserId);
+        this.mockMvc.perform(delete("/api/users/{requestedId}", testUserId)
+                        .with(user(testUserAccount)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenUserIdDoesNotExistAndUserIsAuthenticatedAndAuthorized_thenReturn404() throws Exception {
+
+        UUID testUserId = UUID.randomUUID();
+
+        doThrow(new UserNotFoundException("User not found with id: " + testUserId)).when(userService).deleteUser(testUserId);
+
+        when(userService.getUserById(testUserId)).thenReturn(null);
+
+        this.mockMvc.perform(delete("/api/users/{requestedId}", testUserId)
+                        .with(user(testUserAccount)))
+                .andExpect(status().isNotFound());
     }
 }
--- src/test/java/com/caldev/wishlister/integrationTests/controllers/UserAccountControllerShowTests.java
+++ src/test/java/com/caldev/wishlister/integrationTests/controllers/UserAccountControllerShowTests.java
@@ -13,19 +13,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
