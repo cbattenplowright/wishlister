7a1eceae43a66bdaca8de068db72d512785ca544
Callum BP <callumbp15@gmail.com>
2024-08-30 13:09:38 +0100

    WIS-42-show-wishlist-product-endpoint written findProductById method in ProductService

---
+++ src/main/java/com/caldev/wishlister/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.caldev.wishlister.services;
+
+import com.caldev.wishlister.entities.Product;
+import com.caldev.wishlister.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> findProductById(Long requestedProductId) {
+
+        return productRepository.findById(requestedProductId);
+    }
+}
