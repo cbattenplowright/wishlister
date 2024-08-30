a24e5267d28cbd04fd0bee9b8ef73b580537ecfa
Callum BP <callumbp15@gmail.com>
2024-08-30 15:31:08 +0100

    WIS-41-implement-create-product-endpoint created WishlistProductRepository

---
+++ src/main/java/com/caldev/wishlister/repositories/WishlistProductRepository.java
@@ -0,0 +1,9 @@
+package com.caldev.wishlister.repositories;
+
+import com.caldev.wishlister.entities.WishlistProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishlistProductRepository extends JpaRepository<WishlistProduct, Long> {
+}
