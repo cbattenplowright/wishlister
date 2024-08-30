4192948755595d23be8027d8ff2e6eae69f47fad
Callum BP <callumbp15@gmail.com>
2024-08-30 15:53:55 +0100

    WIS-41-implement-create-product-endpoint 201 endpoint test GREEN

---
+++ src/test/java/com/caldev/wishlister/integrationTests/controllers/ProductController/ProductControllerCreateProductIntegrationTests.java
@@ -0,0 +1,109 @@
+package com.caldev.wishlister.integrationTests.controllers.ProductController;
+
+import com.caldev.wishlister.controllers.ProductController;
+import com.caldev.wishlister.dtos.ProductDto;
+import com.caldev.wishlister.dtos.WishlistDto;
+import com.caldev.wishlister.entities.Authority;
+import com.caldev.wishlister.entities.PrioritySelection;
+import com.caldev.wishlister.entities.Product;
+import com.caldev.wishlister.entities.UserAccount;
+import com.caldev.wishlister.security.CustomUserDetailsService;
+import com.caldev.wishlister.security.SecurityConfig;
+import com.caldev.wishlister.services.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(controllers = ProductController.class)
+public class ProductControllerCreateProductIntegrationTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomUserDetailsService customUserDetailsService;
+
+    @MockBean
+    ProductService productService;
+
+    UUID testUserId;
+
+    UserAccount testUserAccount;
+
+    Long testProductId;
+
+    ProductDto testProductDto;
+
+    ObjectMapper objectMapper;
+
+    String validJsonRequest;
+
+    @BeforeEach
+    void setUp() throws MalformedURLException, JsonProcessingException {
+
+        testUserId = randomUUID();
+
+        testUserAccount = new UserAccount(
+                "testUser@gmail.com",
+                "password",
+                "testName",
+                LocalDate.of(1999, 8, 10),
+                new HashSet<>(List.of(new Authority("ROLE_USER"))));
+
+        testUserAccount.setId(testUserId);
+
+        testProductId = 1L;
+
+        testProductDto = new ProductDto(
+                "testProduct",
+                testUserId,
+                1599,
+                new URL("https://www.testUrl.com"),
+                new URL("https://www.imageUrl.com"),
+                PrioritySelection.NON_URGENT,
+                "description",
+                LocalDate.now(),
+                new ArrayList<>()
+        );
+
+        objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        validJsonRequest = objectMapper.writeValueAsString(testProductDto);
+
+    }
+
+    @Test
+    void shouldReturn201_whenAuthenticatedAndAuthorizedAndCreatingProduct() throws Exception {
+
+        this.mockMvc.perform(post("/api/products/new")
+                .with(user(testUserAccount))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(validJsonRequest))
+                .andExpect(status().isCreated());
+    }
+
+
+}
--- src/test/java/com/caldev/wishlister/unitTests/dtos/ProductDtoUnitTests.java
+++ src/test/java/com/caldev/wishlister/unitTests/dtos/ProductDtoUnitTests.java
@@ -8,125 +8,136 @@ import org.junit.jupiter.api.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductDtoUnitTests {
 
-    private ProductDto productDto;
-    private UUID userId;
+    private ProductDto testProductDto;
+    private UUID testUserId;
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
-        userId = UUID.randomUUID();
-        productDto = new ProductDto("productName", userId, 1, new URL("https://url.com"), new URL("https://imageUrl.com"), PrioritySelection.IMPORTANT, "description", LocalDate.of(2024, 2, 2));
+        testUserId = UUID.randomUUID();
+        testProductDto = new ProductDto(
+                "testProduct",
+                testUserId,
+                1599,
+                new URL("https://www.testUrl.com"),
+                new URL("https://www.imageUrl.com"),
+                PrioritySelection.NON_URGENT,
+                "description",
+                LocalDate.now(),
+                new ArrayList<>()
+        );
     }
 
     @Test
     public void shouldGetProductName() {
-        String productName = productDto.getProductName();
+        String productName = testProductDto.getProductName();
         assertThat(productName).isEqualTo("productName");
     }
 
     @Test
     public void shouldGetUserId() {
-        UUID userId = productDto.getUserId();
-        assertThat(userId).isEqualTo(this.userId);
+        UUID userId = testProductDto.getUserId();
+        assertThat(userId).isEqualTo(this.testUserId);
     }
 
     @Test
     public void shouldGetPrice() {
-        int price = productDto.getPrice();
+        int price = testProductDto.getPrice();
         assertThat(price).isEqualTo(1);
     }
 
     @Test
     public void shouldGetUrl() throws MalformedURLException {
-        URL url = productDto.getUrl();
+        URL url = testProductDto.getUrl();
         assertThat(url).isEqualTo(new URL("https://url.com"));
     }
 
     @Test
     public void shouldGetImageUrl() throws MalformedURLException {
-        URL imageUrl = productDto.getImageUrl();
+        URL imageUrl = testProductDto.getImageUrl();
         assertThat(imageUrl).isEqualTo(new URL("https://imageUrl.com"));
     }
 
     @Test
     public void shouldGetPrioritySelection() {
-        PrioritySelection prioritySelection = productDto.getPrioritySelection();
+        PrioritySelection prioritySelection = testProductDto.getPrioritySelection();
         assertThat(prioritySelection).isEqualTo(PrioritySelection.IMPORTANT);
     }
 
     @Test
     public void shouldGetDescription() {
-        String description = productDto.getDescription();
+        String description = testProductDto.getDescription();
         assertThat(description).isEqualTo("description");
     }
 
     @Test
     public void shouldGetDateAdded() {
-        LocalDate dateAdded = productDto.getDateAdded();
+        LocalDate dateAdded = testProductDto.getDateAdded();
         assertThat(dateAdded).isEqualTo(LocalDate.of(2024, 2, 2));
     }
 
     @Test
     public void shouldSetProductName() {
-        productDto.setProductName("newProductName");
-        assertThat(productDto.getProductName()).isEqualTo("newProductName");
+        testProductDto.setProductName("newProductName");
+        assertThat(testProductDto.getProductName()).isEqualTo("newProductName");
     }
 
     @Test
     public void shouldSetUserId() {
         UUID newUserId = UUID.randomUUID();
-        productDto.setUserId(newUserId);
-        assertThat(productDto.getUserId()).isEqualTo(newUserId);
+        testProductDto.setUserId(newUserId);
+        assertThat(testProductDto.getUserId()).isEqualTo(newUserId);
     }
 
     @Test
     public void shouldSetPrice() {
-        productDto.setPrice(2);
-        assertThat(productDto.getPrice()).isEqualTo(2);
+        testProductDto.setPrice(2);
+        assertThat(testProductDto.getPrice()).isEqualTo(2);
     }
 
     @Test
     public void shouldSetUrl() throws MalformedURLException {
         URL newUrl = new URL("https://newUrl.com");
-        productDto.setUrl(newUrl);
-        assertThat(productDto.getUrl()).isEqualTo(newUrl);
+        testProductDto.setUrl(newUrl);
+        assertThat(testProductDto.getUrl()).isEqualTo(newUrl);
     }
 
     @Test
     public void shouldSetImageUrl() throws MalformedURLException {
         URL newImageUrl = new URL("https://newImageUrl.com");
-        productDto.setImageUrl(newImageUrl);
-        assertThat(productDto.getImageUrl()).isEqualTo(newImageUrl);
+        testProductDto.setImageUrl(newImageUrl);
+        assertThat(testProductDto.getImageUrl()).isEqualTo(newImageUrl);
     }
 
     @Test
     public void shouldSetPrioritySelection() {
         PrioritySelection newPrioritySelection = PrioritySelection.NON_URGENT;
-        productDto.setPrioritySelection(newPrioritySelection);
-        assertThat(productDto.getPrioritySelection()).isEqualTo(newPrioritySelection);
+        testProductDto.setPrioritySelection(newPrioritySelection);
+        assertThat(testProductDto.getPrioritySelection()).isEqualTo(newPrioritySelection);
     }
 
     @Test
     public void shouldSetDescription() {
-        productDto.setDescription("newDescription");
-        assertThat(productDto.getDescription()).isEqualTo("newDescription");
+        testProductDto.setDescription("newDescription");
+        assertThat(testProductDto.getDescription()).isEqualTo("newDescription");
     }
 
     @Test
     public void shouldSetDateAdded() {
         LocalDate newDateAdded = LocalDate.of(2022, 2, 2);
-        productDto.setDateAdded(newDateAdded);
-        assertThat(productDto.getDateAdded()).isEqualTo(newDateAdded);
+        testProductDto.setDateAdded(newDateAdded);
+        assertThat(testProductDto.getDateAdded()).isEqualTo(newDateAdded);
     }
 
     @Test
     public void shouldToString(){
-        assertThat(productDto.toString()).isEqualTo("ProductDto{productName='productName', userId=" + userId + ", price=1, url=https://url.com, imageUrl=https://imageUrl.com, prioritySelection=IMPORTANT, description='description', dateAdded=2024-02-02}");
+        assertThat(testProductDto.toString()).isEqualTo("ProductDto{productName='productName', userId=" + testUserId + ", price=1, url=https://url.com, imageUrl=https://imageUrl.com, prioritySelection=IMPORTANT, description='description', dateAdded=2024-02-02}");
 
     }
 
