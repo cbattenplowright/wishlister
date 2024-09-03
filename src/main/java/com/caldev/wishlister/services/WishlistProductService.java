b64165919bbca6ba52acc9871f1e576de4c95d79
Callum BP <callumbp15@gmail.com>
2024-09-03 12:48:47 +0100

    WIS-149-index-wishlist-product-endpoint written getAllWishlistProducts() method

--- src/main/java/com/caldev/wishlister/services/WishlistProductService.java
+++ src/main/java/com/caldev/wishlister/services/WishlistProductService.java
@@ -1,4 +1,21 @@
 package com.caldev.wishlister.services;
 
+import com.caldev.wishlister.entities.WishlistProduct;
+import com.caldev.wishlister.repositories.WishlistProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WishlistProductService {
+
+    private final WishlistProductRepository wishlistProductRepository;
+
+    public WishlistProductService(WishlistProductRepository wishlistProductRepository) {
+        this.wishlistProductRepository = wishlistProductRepository;
+    }
+
+    public List<WishlistProduct> getAllWishlistProducts() {
+        return wishlistProductRepository.findAll();
+    }
 }
