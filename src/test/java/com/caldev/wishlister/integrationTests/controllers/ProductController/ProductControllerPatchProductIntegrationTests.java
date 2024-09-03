7cc73672ecb539da7c21f0b530343433fe384cc9
Callum BP <callumbp15@gmail.com>
2024-09-03 12:04:37 +0100

    WIS-43-implement-update-product-detail-endpoint 200 status code test GREEN

--- src/test/java/com/caldev/wishlister/integrationTests/controllers/ProductController/ProductControllerPatchProductIntegrationTests.java
+++ src/test/java/com/caldev/wishlister/integrationTests/controllers/ProductController/ProductControllerPatchProductIntegrationTests.java
@@ -13,10 +13,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.net.MalformedURLException;
@@ -28,6 +30,11 @@ import java.util.List;
 import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(SecurityConfig.class)
 @WebMvcTest(controllers = ProductController.class)
@@ -49,6 +56,8 @@ public class ProductControllerPatchProductIntegrationTests {
     Long testProductId;
 
     ProductDto testProductDto;
+
+    Product testProduct;
 
     ObjectMapper objectMapper;
 
@@ -73,8 +82,8 @@ public class ProductControllerPatchProductIntegrationTests {
                 "testProduct",
                 testUserId,
                 1599,
-                new URL("testUrl"),
-                new URL("testImageUrl"),
+                new URL("https://www.testUrl.com"),
+                new URL("https://www.testImageUrl.com"),
                 PrioritySelection.IMPORTANT,
                 "testDescription",
                 LocalDate.now(),
@@ -84,5 +93,30 @@ public class ProductControllerPatchProductIntegrationTests {
         objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
         validJsonRequest = objectMapper.writeValueAsString(testProductDto);
+
+        testProduct = new Product(
+                "testProduct",
+                156,
+                new URL("https://www.testUrl.com"),
+                new URL("https://www.imageUrl.com"),
+                "testDescription",
+                PrioritySelection.IMPORTANT,
+                LocalDate.now(),
+                new ArrayList<>(),
+                testUserAccount
+        );
+    }
+
+    @Test
+    void shouldReturn200_whenRequestIsValid() throws Exception {
+
+        when(productService.updateProduct(any(Long.class), any(ProductDto.class), any(UserAccount.class))).thenReturn(testProduct);
+
+        this.mockMvc.perform(patch("/api/products/{requestedUserId}/{requestedProductId}", testUserId, testProductId)
+                .with(user(testUserAccount))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(validJsonRequest))
+                .andExpect(status().isOk());
+
     }
 }
