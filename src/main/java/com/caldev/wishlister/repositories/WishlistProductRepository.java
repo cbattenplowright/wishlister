be50c5815b95f5205c61ee0d371f3a850111acfe
Callum BP <callumbp15@gmail.com>
2024-09-05 13:46:00 +0100

    WIS-152-create-wishlist-product-endpoint fixed repository query to scan Wishlist and Product repositories

--- src/main/java/com/caldev/wishlister/repositories/WishlistProductRepository.java
+++ src/main/java/com/caldev/wishlister/repositories/WishlistProductRepository.java
@@ -2,10 +2,10 @@ package com.caldev.wishlister.repositories;
 
 import com.caldev.wishlister.entities.WishlistProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistProductRepository extends JpaRepository<WishlistProduct, Long> {
-
-    boolean existsByWishlistIdAndProductId(Long wishlistId, Long productId);
+    boolean existsByWishlist_WishlistIdAndProduct_ProductId(Long wishlistId, Long productId);
 }
--- src/main/java/com/caldev/wishlister/services/WishlistProductService.java
+++ src/main/java/com/caldev/wishlister/services/WishlistProductService.java
@@ -1,5 +1,7 @@
 package com.caldev.wishlister.services;
 
+import com.caldev.wishlister.dtos.WishlistProductDto;
+import com.caldev.wishlister.entities.UserAccount;
 import com.caldev.wishlister.entities.WishlistProduct;
 import com.caldev.wishlister.repositories.WishlistProductRepository;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,10 @@ public class WishlistProductService {
     }
 
     public boolean existsByWishlistIdAndProductId(Long wishlistId, Long productId) {
-        return wishlistProductRepository.existsByWishlistIdAndProductId(wishlistId, productId);
+        return wishlistProductRepository.existsByWishlist_WishlistIdAndProduct_ProductId(wishlistId, productId);
+    }
+
+    public WishlistProduct createWishlistProduct(WishlistProductDto newWishlistProductDto, UserAccount testUserAccount) {
+        return null;
     }
 }
