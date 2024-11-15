2f2c495ef66c32d2caac3acec52d5be1e57a238f
Callum BP <callumbp15@gmail.com>
2024-11-15 11:39:27 +0000

    INDEX User SharedWishlists endpoint written

--- src/main/java/com/caldev/wishlister/controllers/SharedWishlistController.java
+++ src/main/java/com/caldev/wishlister/controllers/SharedWishlistController.java
@@ -2,15 +2,19 @@ package com.caldev.wishlister.controllers;
 
 import com.caldev.wishlister.dtos.SharedWishlistDto;
 import com.caldev.wishlister.entities.SharedWishlist;
+import com.caldev.wishlister.entities.UserAccount;
 import com.caldev.wishlister.services.SharedWishlistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/shared-wishlists")
@@ -40,6 +44,34 @@ public class SharedWishlistController {
             return new ResponseEntity<>(sharedWishlistDtoList, HttpStatus.OK);
         }
 
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<>("No wishlists not found", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/{requestedUserId}")
+    @PreAuthorize("hasRole('ADMIN') || hasRole('USER') && #userAccount.id == #requestedUserId")
+    public ResponseEntity<Object> getSharedWishlistsByUser(
+            @PathVariable UUID requestedUserId,
+            @AuthenticationPrincipal UserAccount userAccount) {
+
+        if (userAccount == null) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        List<SharedWishlist> sharedWishlistList = sharedWishlistService.findAllSharedWishlistsByUser();
+
+        if (sharedWishlistList != null) {
+
+            List<SharedWishlistDto> sharedWishlistDtoList = sharedWishlistList
+                    .stream()
+                    .map(sharedWishlist -> new SharedWishlistDto(
+                            sharedWishlist.getSharedWishlistId(),
+                            sharedWishlist.getSharedUser().getUserAccountId(),
+                            sharedWishlist.getSharedWishlist().getWishlistId())
+                    ).toList();
+
+            return new ResponseEntity<>(sharedWishlistDtoList, HttpStatus.OK);
+        }
+
+        throw new SharedWishlistsNotFoundException("Shared wishlists not found");
     }
 }
